fd79d55c40be9128b20bdc32b3069cad71531776
Slava <Tsvetkov@mail.ru>
2023-08-06 17:37:34 +0300

    initialized new array and check input value

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+//  либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+//  пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+int Start(string message)
+{
+    System.Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+bool ValidateNumber(int value)
+{
+    if (value <= 0)
+    {
+        Console.WriteLine("Количество элементов массива не может быть отрицательным, либо равно нулю." +
+        "\n Попробуйте заново. ");
+        return false;
+    }
+    return true;
+}
+
+int N = Start("Введите число элементов массива");
+Console.WriteLine();
+string[] stringArray = new string[N];
+
+if (ValidateNumber(N))
+{
+  
+}
